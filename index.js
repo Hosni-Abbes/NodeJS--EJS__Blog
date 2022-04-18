a56ba2b618b0f4093b237073cce19a70b905867c
Mr-h2 <hosny.abbes@gmail.com>
2022-04-18 14:50:17 +0100

    Add Mongodb Database to Store Data

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const _ = require('lodash');
+const mongoose = require('mongoose');
+const env = require('dotenv').config();
 
 const app = express();
 
@@ -10,15 +12,39 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname+'/public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
+//Database Connect
+mongoose.connect(process.env.DB_CONNECT)
+.then(() => console.log("Database connected"))
+.catch(err => console.log(err))
 
-//all posts
-const posts = [];
+//create mongoose Schema
+const postSchema = new mongoose.Schema({
+  title:{
+    type:String,
+    required:true
+  },
+  desc:{
+    type:String,
+    required:true
+  }
+});
+
+//Create Post Model
+const Post = mongoose.model('Post', postSchema);
+
 
 
 //get home page
 app.get('/', (req, res)=>{
   res.locals.title = 'Home';
-  res.render('index', {posts});
+  //get posts from database
+  const allPosts = Post.find((err, postsFounded)=>{
+    if(err){
+      res.send(err);
+    }else{
+      res.render('index', {postsFounded});
+    }
+  })
 })
 
 //get about page
@@ -41,10 +67,17 @@ app.get('/create', (req, res)=>{
 
 
 //get post page when click on read more btn
-app.get('/posts/:postTitle', (req, res)=>{
-  const post = posts.find(post => _.lowerCase(post.title) === _.lowerCase(req.params.postTitle));
-  res.locals.title = req.params.postTitle;
-  res.render('post', {post});
+app.get('/posts/:postId', (req, res)=>{
+  //using mongoose find
+  const findPost = Post.findById(req.params.postId, (err, postFounded)=>{
+    if(err){
+      res.send(err);
+    }else{
+      res.locals.title = postFounded.title;
+      res.render('post', {postFounded});
+    }
+  })
+
 })
 
 //Create new post
@@ -52,13 +85,15 @@ app.post('/create', (req, res)=>{
   const postTitle = req.body.postTitle;
   const postDesc= req.body.postDesc; 
   if(postTitle !=='' && postDesc !==''){
-    posts.push({
+    //Add The Post To Database
+    const post = new Post({
       title: postTitle,
       desc: postDesc
     });
+    post.save();
   }
   res.redirect('/');
 })
 
 
-app.listen(5000, () => console.log("Server Running") );
+app.listen(process.env.PORT, () => console.log("Server Running") );
